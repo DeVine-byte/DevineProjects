64d538101fcaebb3816ad908d3cd2ceb52d9a3b3
DeVine-byte <Divine4529@gmail.com>
2026-06-09 12:56:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
-from fastapi.responses import HTMLResponse
 
 from Keyholder.app import app as keyholder_app
 from expense_tracker.main import app as expense_tracker_app
@@ -8,12 +8,9 @@ from videoCompressor.app import app as vid_app
 
 app = FastAPI()
 
-
-
-@app.get("/", response_class=HTMLResponse)
-async def home():
-    with open("frontend/index.html") as f:
-        return f.read()
+@app.get("/")
+async def root():
+    return {"message": "All services running"}
 
 app.mount(
     "/keyholder",
