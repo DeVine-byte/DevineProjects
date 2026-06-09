a59299122fff08c0102f982a6db6cfb7dfe6d7fc
DeVine-byte <Divine4529@gmail.com>
2026-06-09 22:19:04 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -48,26 +48,6 @@ window.addEventListener(
 
 revealSections();
 
-const menuBtn =
-document.querySelector(".menu-btn");
-
-const navLinks =
-document.querySelector(".nav-links");
-
-if(menuBtn){
-
-    menuBtn.addEventListener(
-        "click",
-        () => {
-
-            navLinks.classList.toggle(
-                "active"
-            );
-
-        }
-    );
-
-}
 
 // Cursor
 
