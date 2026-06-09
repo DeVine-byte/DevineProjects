d6110e643e174e0fd6a7506e06b15357ae59527b
DeVine-byte <Divine4529@gmail.com>
2026-06-09 12:50:38 +0100

    Serve HTML from home endpoint instead of JSON
    Replace root endpoint with home endpoint serving HTML.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from Keyholder.app import app as keyholder_app
 from expense_tracker.main import app as expense_tracker_app
@@ -7,9 +10,12 @@ from videoCompressor.app import app as vid_app
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "All services running"}
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    with open("frontend/index.html") as f:
+        return f.read()
 
 app.mount(
     "/keyholder",
