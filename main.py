aa4e1061c23806eebca1b2d49e57fd5293397ddd
DeVine-byte <Divine4529@gmail.com>
2026-06-09 12:53:32 +0100

    Clean up unused FastAPI imports
    Removed unused imports for StaticFiles and Jinja2Templates.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 from Keyholder.app import app as keyholder_app
